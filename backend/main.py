a700f442860a380c9c35afb9dd7e5f9f70579bff
007jbks <kartiksirohi383@gmail.com>
2025-10-15 22:35:23 +0530

    Mongo client setup complete

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from routers import auth
-from mongo import connect
-app = FastAPI()
-connect()
+from mongo import connect_to_mongo, close_mongo_connection
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await connect_to_mongo()
+    yield
+    await close_mongo_connection()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth.router)
 
--- backend/mongo.py
+++ backend/mongo.py
@@ -1,15 +1,34 @@
-from dotenv import load_dotenv
-from pymongo import MongoClient
 import os
 import logging
+from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+
+logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 
 mongo_srv = os.environ["mongo"]
 
-def connect():
+async def connect_to_mongo():
+    logging.info("inside the connection function")
+    if not mongo_srv:
+        logging.error("Error: 'mongo' environment variable not set.")
+        return
+
+    logging.info("Connecting to MongoDB...")
     try:
-        c = MongoClient(mongo_srv)
-        logging.info("Connected")
+        client = AsyncIOMotorClient(mongo_srv)
+        logging.info("Successfully connected to MongoDB!")
     except Exception as e:
-        print(f"Error {e}")
+        logging.error(f"Could not connect to MongoDB: {e}")
+
+async def close_mongo_connection():
+    logging.info("Closing MongoDB connection...")
+    if client:
+        client.close()
+    logging.info("MongoDB connection closed.")
+
+def get_database() -> AsyncIOMotorDatabase:
+    if client.db is None:
+        raise Exception("Database not initialized. Call connect_to_mongo first.")
+    return client.db
