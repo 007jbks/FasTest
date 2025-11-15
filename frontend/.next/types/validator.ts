2691a72eb6e11b6cd4765b1f5055f42b521f977b
007jbks <kartiksirohi383@gmail.com>
2025-11-15 23:07:03 +0530

    Fixed acccounts error

--- frontend/.next/types/validator.ts
+++ frontend/.next/types/validator.ts
@@ -45,15 +45,6 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
-// Validate ../../app/acccount/page.tsx
-{
-  type __IsExpected<Specific extends AppPageConfig<"/acccount">> = Specific
-  const handler = {} as typeof import("../../app/acccount/page.js")
-  type __Check = __IsExpected<typeof handler>
-  // @ts-ignore
-  type __Unused = __Check
-}
-
 // Validate ../../app/account/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/account">> = Specific
